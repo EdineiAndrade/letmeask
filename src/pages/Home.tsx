a1603665087e098d06dbfd1c67e9cc52985255d7
EdineiAndrade <edinei22.ba@gmail.com>
2021-06-25 00:02:10 -0300

    commit Aula3 pt 1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { useHistory } from 'react-router';
+import { FormEvent, useState } from 'react';
 
-import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { database } from '../services/firebase';
+
+import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
@@ -13,15 +16,31 @@ import '../styles/auth.scss';
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth()
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
-
         if (!user) {
             await signInWithGoogle()
         }
         history.push('/rooms/new');
-
     }
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+        // eslint-disable-next-line
+        const roomRef = await database.ref(`/rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.')
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
+    }
+
 
     return (
         <div id="page-auth">
@@ -38,10 +57,12 @@ export function Home() {
                         Crie sua sala com  Google
                     </button>
                     <div className="separator">Ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
